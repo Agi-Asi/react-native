cc1bcbc62c88d307a6d7a6e343c6a02dbd8fa81a
Riccardo Cipolleschi <cipolleschi@meta.com>
2026-08-19 15:18:15 +0100

    Process font variation settings in native view configs

---
+++ packages/react-native/Libraries/ReactNative/__tests__/getNativeComponentAttributes-test.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
+jest.mock('../UIManager', () => ({
+  __esModule: true,
+  default: {
+    getConstants: () => ({ViewManagerNames: []}),
+    getDefaultEventTypes: () => ({}),
+    getViewManagerConfig: name =>
+      name === 'TestView'
+        ? {
+            NativeProps: {fontVariationSettings: 'String'},
+            bubblingEventTypes: {},
+            directEventTypes: {},
+          }
+        : null,
+  },
+}));
+
+const getNativeComponentAttributes =
+  require('../getNativeComponentAttributes').default;
+const {
+  fontVariationSettingsAttribute,
+} = require('../../Components/View/ReactNativeStyleAttributes');
+
+describe('getNativeComponentAttributes', () => {
+  it('processes object font variation settings from native view configs', () => {
+    const viewConfig = getNativeComponentAttributes('TestView');
+
+    expect(viewConfig.validAttributes.fontVariationSettings).toEqual(
+      fontVariationSettingsAttribute,
+    );
+    expect(
+      viewConfig.validAttributes.fontVariationSettings.process({
+        wght: 552.5,
+        opsz: 17.25,
+      }),
+    ).toBe("'opsz' 17.25, 'wght' 552.5");
+  });
+});
