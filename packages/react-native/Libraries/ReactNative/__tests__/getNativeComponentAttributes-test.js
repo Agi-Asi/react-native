bd55f0aa422610d381fd5064bd4ab77611fa9f55
Riccardo Cipolleschi <cipolleschi@meta.com>
2026-08-19 15:24:45 +0100

    Fix native view config test lint

--- packages/react-native/Libraries/ReactNative/__tests__/getNativeComponentAttributes-test.js
+++ packages/react-native/Libraries/ReactNative/__tests__/getNativeComponentAttributes-test.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  */
 
@@ -25,11 +26,11 @@ jest.mock('../UIManager', () => ({
   },
 }));
 
-const getNativeComponentAttributes =
-  require('../getNativeComponentAttributes').default;
 const {
   fontVariationSettingsAttribute,
 } = require('../../Components/View/ReactNativeStyleAttributes');
+const getNativeComponentAttributes =
+  require('../getNativeComponentAttributes').default;
 
 describe('getNativeComponentAttributes', () => {
   it('processes object font variation settings from native view configs', () => {
